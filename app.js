cacc7f10197a72b29c26f111b4c3cad72475d661
fand <fand>
2014-05-06 21:19:52 +0900

    moved js dir

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ var index = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -41,10 +37,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+        res.send({message: err.message});
     });
 }
 
@@ -52,10 +45,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    res.send({message: err.message});
 });
 
 module.exports = app;
