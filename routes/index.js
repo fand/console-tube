e1890e92bfa1ffad8f5d42bfe4c4925ccfa39f1c
fand <fand>
2014-05-11 10:54:31 +0900

    Put info for auth to auth.json

--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,24 @@ var fs = require('fs');
 router.get('/:url', function(req, res) {
   var url = decodeURIComponent(req.params.url);
 
-  var options = ['-q', '-o', '-', url];
-  if (url.match(/nicovideo\.jp/)) {    // NicoNico
-    var username = 'XXXXXXXX';
-    var password = 'XXXXXXXX';
-    var auth = ['--username', username, '--password', password];
-    options = auth.concat(options);
+  fs.readFile('./auth.json', function (err, data) {
+    var auths = JSON.parse(data);
+
+    var options = login(url, auths);
+    options = options.concat(['-q', '-o', '-', url]);
+
     var youtube = spawn('youtube-dl', options);
     var command = new FFmpeg({source: youtube.stdout})
           .withSize('320x240')
           .toFormat('webm')
+          .on('start',function () {
+            console.log('start');
+          })
+          .on('end',function () {
+            console.log('end');
+          })
           .writeToStream(res);
-  } else {
-    var youtube = spawn('youtube-dl', options);
-    youtube.stdout.pipe(res);
-  }
+  });
 });
 
 module.exports = router;
@@ -33,3 +36,13 @@ var sys = require('sys');
 process.on('uncaughtException', function(err) {
   sys.puts(err.stack);
 });
+
+var login = function (url, auths) {
+  var hostname = require('url').parse(url).hostname;
+  for (var host in auths) {
+    if (hostname.match(host)) {
+      return auths[host];
+    }
+  }
+  return [];
+};
