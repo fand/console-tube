56bc3831e55944fdff91033b7476bcdc66e0ab15
fand <fand>
2014-05-13 19:15:46 +0900

    package done

--- app.js
+++ app.js
@@ -17,17 +17,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    return next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return next();
 });
 
 app.use('/', index);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -35,17 +35,17 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.send({message: err.message});
-    });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send({message: err.message});
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send({message: err.message});
+  res.status(err.status || 500);
+  res.send({message: err.message});
 });
 
 module.exports = app;
